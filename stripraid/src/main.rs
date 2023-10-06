6327810bac1b94eca329b358ef655ed953678441
Delan Azabani <delan@azabani.com>
2023-10-06 14:06:56 +0800

    stripraid: handle read errors

--- stripraid/src/main.rs
+++ stripraid/src/main.rs
@@ -1,18 +1,32 @@
 use std::{fs::File, io::{self, BufReader, Read, stdout, Write, Seek}, error::Error, time::Instant};
 
-use rayon::prelude::{IntoParallelRefMutIterator, ParallelIterator, IntoParallelRefIterator, IndexedParallelIterator};
+use rayon::prelude::{IntoParallelRefMutIterator, ParallelIterator};
 
 const STRIPE: usize = 262144;
-const SKIP: usize = 500000 * 1048576 / STRIPE / 12;
-const SEEK: usize = SKIP * 12;
+const SKIP_ROWS: usize = 4753000 * 1048576 / STRIPE / 12;
+const _YOU_SHOULD_SEEK_DD_BY_THIS_MANY_STRIPES: usize = SKIP_ROWS * 12;
+
+macro_rules! warn {
+    ($sink:expr, $fmt:literal $(, $arg:expr)* $(,)*) => {{
+        let msg = format!($fmt $(, $arg)*);
+        writeln!($sink, "{}", msg)?;
+        eprintln!("{}", msg);
+    }};
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut log = File::options()
+        .read(true)
+        .write(true)
+        .create_new(true)
+        .open(std::env::var("STRIPRAID_LOG")?)?;
+
     let mut out = stdout().lock();
     let mut files = std::env::args().skip(1)
         .map(File::open)
         .collect::<io::Result<Vec<_>>>()?;
     for file in files.iter_mut() {
-        file.seek(io::SeekFrom::Start((SKIP * STRIPE).try_into()?))?;
+        file.seek(io::SeekFrom::Start((SKIP_ROWS * STRIPE).try_into()?))?;
     }
 
     let files = files.iter()
@@ -25,51 +39,109 @@ fn main() -> Result<(), Box<dyn Error>> {
         .zip(datas.into_iter())
         .collect::<Vec<_>>();
 
-    let mut first_error = None;
     let mut q = pairs.len() - 2;
     let mut p = pairs.len() - 1;
     for row in 0.. {
-        if row >= SKIP {
+        if row >= SKIP_ROWS {
             let t1 = Instant::now();
+
             let results = pairs.par_iter_mut()
                 .map(|(file, data)| {
                     file.read_exact(data)
                 })
                 .collect::<Vec<_>>();
-            for result in results {
-                result?; // :)
-            }
-            let t2 = Instant::now();
-            let xor = pairs./*par_*/iter()
-                .enumerate()
-                .filter(|(i, _)| *i != q)
-                .map(|(_, (_, data))| data)
-                .cloned()
-                .reduce(/*|| vec![0u8; STRIPE],*/ |p, q| {
-                    let mut result = vec![0u8; STRIPE];
-                    for i in 0..result.len() {
-                        result[i] = p[i] ^ q[i];
+            let mut q_failed = false;
+            let mut p_failed = false;
+            let mut data_failed = None;
+            for (i, result) in results.iter().enumerate() {
+                if let Err(error) = result {
+                    let kind = if i == q {
+                        "q"
+                    } else if i == p {
+                        "p"
+                    } else {
+                        "data"
+                    };
+                    warn!(
+                        log, "warning: {:?}\nwarning: read error at row {} disk {} ({})",
+                        error, row, i, kind,
+                    );
+                    if i == q {
+                        q_failed = true;
+                    } else if i == p {
+                        p_failed = true;
+                    } else {
+                        if data_failed.replace(i).is_some() {
+                            panic!("fatal: two data read errors in the same row");
+                        }
                     }
-                    result
-                })
-                .unwrap();
-            if let Some((offset, _)) = xor.iter().enumerate().find(|(_, x)| **x != 0) {
-                first_error = first_error.or(Some((row, offset)));
-                let (first_row, first_offset) = first_error.unwrap();
-                let size = first_row * STRIPE * (pairs.len() - 2) / 1024;
-                eprintln!(
-                    "warning: bad parity at ({}, {}); first error at ({}, {}) or logical {} KiB",
-                    row, offset, first_row, first_offset, size
-                );
+                }
             }
+
+            let t2 = Instant::now();
+
+            if q_failed {
+                warn!(log, "warning: no action needed; q is not yet implemented");
+            }
+
+            if p_failed && data_failed.is_some() {
+                panic!("fatal: p and data read errors in the same row");
+            } else if let Some(f) = data_failed {
+                // restore failed data from xor parity (p)
+                warn!(log, "warning: restoring data from xor parity (p)");
+                let xor = pairs.iter()
+                    .enumerate()
+                    .filter(|(i, _)| *i != q && *i != f)
+                    .map(|(_, (_, data))| data)
+                    .cloned()
+                    .reduce(|p, q| {
+                        let mut result = vec![0u8; STRIPE];
+                        for i in 0..result.len() {
+                            result[i] = p[i] ^ q[i];
+                        }
+                        result
+                    })
+                    .unwrap();
+                pairs[f].1.copy_from_slice(&xor);
+            }
+
+            if p_failed {
+                warn!(log, "warning: no action needed; we are stripping p anyway");
+            } else {
+                // check xor parity (p)
+                let xor = pairs.iter()
+                    .enumerate()
+                    .filter(|(i, _)| *i != q)
+                    .map(|(_, (_, data))| data)
+                    .cloned()
+                    .reduce(|p, q| {
+                        let mut result = vec![0u8; STRIPE];
+                        for i in 0..result.len() {
+                            result[i] = p[i] ^ q[i];
+                        }
+                        result
+                    })
+                    .unwrap();
+                if let Some((offset, _)) = xor.iter().enumerate().find(|(_, x)| **x != 0) {
+                    warn!(
+                        log, "warning: bad parity at row {} offset {}; data may be corrupt!",
+                        row, offset,
+                    );
+                }
+            }
+
             let t3 = Instant::now();
+
             for (i, (_, data)) in pairs.iter().enumerate() {
                 if i != p && i != q {
                     out.write_all(data)?;
                 }
             }
             if row % 300 == 0 {
-                eprintln!("\rrow {}: read {:?}, xor {:?}, write {:?}", row, t2 - t1, t3 - t2, Instant::now() - t3);
+                eprintln!(
+                    "\rrow {}: read {:?}, xor {:?}, write {:?}",
+                    row, t2 - t1, t3 - t2, Instant::now() - t3,
+                );
             }
         }
         q = if q == 0 {
